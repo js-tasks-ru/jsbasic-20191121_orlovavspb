dc838dadc808b28e5b03061ba84d8f1bcf385544
webyoga <webyoga@yandex.ru>
2019-12-08 05:45:12 +0300

    reshenie modul 3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,20 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let a1 = 0;
+  let a2 = 0;
+  for (let prop in obj) {
+    a1++;
+    if (typeof obj[prop] == 'undefined' || obj[prop] == null || typeof obj[prop] == 'object') {
+      a2++;
+    }
+  }
+  if (a1 > 0 && a2 === 0) {
+    let copy = Object.assign({}, obj);
+    return copy;
+  }  
+  else {
+    return false;
+  }
 }
+  
